a475c9112c5366f7b7f4ec24fd2bfb5f0a8075ba
Md Sirajul Islam Sojib <sirajulislam1123@gmail.com>
2020-11-24 01:59:07 +0600

    Route: default routing done.

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,20 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const admin = require('firebase-admin');
+const express = require('express');
+const functions = require('firebase-functions');
+const cors = require('cors'); //CORS(Cross-Origin Resource Sharing) is a node. js package for providing a Connect/Express middleware that can be used to enable CORS with various options. 
+const app = express();
+
+
+//Routes
+app.get('/',(req,res) =>{
+    return res.status(200).send('Hello World');
+} ) //base url hit korle "hello world" output dekhabe.
+
+
+//export the api to firebase cloud functions
+exports.app = functions.https.onRequest(app); //handle all api request
